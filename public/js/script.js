c6ad393b3c4fac82ab2fd2e6f94a49a8d1943d5a
RaspiberryPi <douglas.costa11@fatec.sp.gov.br>
2023-12-04 09:06:32 -0300

    att

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,57 @@
 
-const inputFile = document.getElementById('inputFile');
-const inputImage = document.getElementById('inputImage');
-const outputCanvas = document.getElementById('outputCanvas');
+function loadImage() {
+    const inputFile = document.getElementById('inputFile');
+    const inputCanvas = document.getElementById('inputCanvas');
+    const ctx = inputCanvas.getContext('2d');
 
-inputFile.addEventListener('change', ()=>{
     const file = inputFile.files[0];
-    if(file){
-        const reader = new FileReader();
-        reader.onload = function(e){
-            inputImage.src = e.target.reader;
-        };
-        reader.readAsDataURL(file);'
-    }
-})
+    const reader = new FileReader();
 
-(async ()=>{
-    //codigo
-})()
+    reader.onload = function (e) {
+        const img = new Image();
+        img.src = e.target.result;
+
+        img.onload = function () {
+            // Draw the image on the canvas
+            ctx.drawImage(img, 0, 0, inputCanvas.width, inputCanvas.height);
+
+            // Call function to convert to grayscale
+            convertToGrayscale();
+        };
+    };
+
+    reader.readAsDataURL(file);
+}
+
+function convertToGrayscale() {
+    const inputCanvas = document.getElementById('inputCanvas');
+    const ctx = inputCanvas.getContext('2d');
+
+    // Get image data from the canvas
+    const imageData = ctx.getImageData(0, 0, inputCanvas.width, inputCanvas.height);
+    const data = imageData.data;
+
+    // Convert each pixel to grayscale
+    for (let i = 0; i < data.length; i += 4) {
+        const red = data[i];
+        const green = data[i + 1];
+        const blue = data[i + 2];
+
+        // Convert to grayscale using luminosity formula
+        const grayscale = 0.299 * red + 0.587 * green + 0.114 * blue;
+
+        // Set the pixel values to grayscale
+        data[i] = grayscale;
+        data[i + 1] = grayscale;
+        data[i + 2] = grayscale;
+    }
+
+    // Put the modified image data back on the canvas
+    ctx.putImageData(imageData, 0, 0);
+}
+
+// Auto-executable function
+(function autoExecute() {
+    // Your code to be executed automatically after the page loads
+})();
+
