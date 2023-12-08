c34cfb15e26dc6f6d2d5783d1e66d9c67f93c8f7
RaspiberryPi <douglas.costa11@fatec.sp.gov.br>
2023-12-08 07:03:11 -0300

    2023-12-8: Trabalho concluido com face-api

--- public/js/script.js
+++ public/js/script.js
@@ -1,57 +1,64 @@
+const fileInput = document.getElementById('file');
+const imagem = document.getElementById('imagem');
+let canvas; // Declare a variável canvas fora da função para que possa ser acessada globalmente
 
-function loadImage() {
-    const inputFile = document.getElementById('inputFile');
-    const inputCanvas = document.getElementById('inputCanvas');
-    const ctx = inputCanvas.getContext('2d');
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(() => {
+    fileInput.addEventListener('change', (event) => {
+        const file = event.target.files[0];
+        console.log('Arquivo selecionado', file);
+        if (file) {
+            const imagemURL = URL.createObjectURL(file);
+            imagem.src = imagemURL;
+            imagem.onload = reconhecimentoFacial; // Iniciar o reconhecimento apenas após a imagem ser carregada
+        }
+    });
 
-    const file = inputFile.files[0];
-    const reader = new FileReader();
+    async function reconhecimentoFacial() {
+        console.log('Fazendo o reconhecimento facial!');
+        try {
+            // Remover o canvas anterior, se existir
+            if (canvas) {
+                canvas.remove();
+            }
 
-    reader.onload = function (e) {
-        const img = new Image();
-        img.src = e.target.result;
+            // Criar um novo canvas
+            canvas = faceapi.createCanvasFromMedia(imagem);
 
-        img.onload = function () {
-            // Draw the image on the canvas
-            ctx.drawImage(img, 0, 0, inputCanvas.width, inputCanvas.height);
+            // Encontrar o contêiner do Bootstrap (ajuste o seletor conforme necessário)
+            const containerBootstrap = document.querySelector('.container');
 
-            // Call function to convert to grayscale
-            convertToGrayscale();
-        };
-    };
+            // Anexar o canvas ao contêiner do Bootstrap
+            containerBootstrap.appendChild(canvas);
 
-    reader.readAsDataURL(file);
-}
+            // Alinhar o canvas com a imagem
+            canvas.style.position = 'absolute';
+            canvas.style.top = `${imagem.offsetTop}px`;
+            canvas.style.left = `${imagem.offsetLeft + 10}px`;
 
-function convertToGrayscale() {
-    const inputCanvas = document.getElementById('inputCanvas');
-    const ctx = inputCanvas.getContext('2d');
+            const displaySize = { width: imagem.width, height: imagem.height };
 
-    // Get image data from the canvas
-    const imageData = ctx.getImageData(0, 0, inputCanvas.width, inputCanvas.height);
-    const data = imageData.data;
+            faceapi.matchDimensions(canvas, displaySize);
 
-    // Convert each pixel to grayscale
-    for (let i = 0; i < data.length; i += 4) {
-        const red = data[i];
-        const green = data[i + 1];
-        const blue = data[i + 2];
+            // Bloco sem o timer
+            const detections = await faceapi.detectAllFaces(
+                imagem,
+                new faceapi.TinyFaceDetectorOptions() // Fornecendo explicitamente as opções do TinyFaceDetector
+            )
+                .withFaceLandmarks()
+                .withFaceExpressions();
 
-        // Convert to grayscale using luminosity formula
-        const grayscale = 0.299 * red + 0.587 * green + 0.114 * blue;
+            const resizedDetections = faceapi.resizeResults(detections, displaySize);
 
-        // Set the pixel values to grayscale
-        data[i] = grayscale;
-        data[i + 1] = grayscale;
-        data[i + 2] = grayscale;
+            faceapi.draw.drawDetections(canvas, resizedDetections); // Desenhando detecções
+            //faceapi.draw.drawFaceLandmarks(canvas, resizedDetections); // Desenhando os pontos de referencia
+            faceapi.draw.drawFaceExpressions(canvas, resizedDetections); // Desenhando expressões
+        } catch (error) {
+            console.log(error);
+        }
     }
-
-    // Put the modified image data back on the canvas
-    ctx.putImageData(imageData, 0, 0);
-}
-
-// Auto-executable function
-(function autoExecute() {
-    // Your code to be executed automatically after the page loads
-})();
-
+});
